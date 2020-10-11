e59c89efb89fafcfc2a6a627b62cff7b50303fc6
3A <524019351@qq.com>
2020-10-11 21:31:27 +0800

    没写的函数

--- pos.h
+++ pos.h
@@ -1,8 +1,16 @@
 #pragma once
 
-struct pos
+class pos
 {
     float x;
     float y;
-    pos(float x,float y):x(x),y(y){}
+    pos(float x,float y):x(x),y(y){};
+
+    pos toS();
+    pos toW();
+    pos toE();
+    pos toFN();
+    pos toFS();
+    pos toFW();
+    pos toFE();
 };
